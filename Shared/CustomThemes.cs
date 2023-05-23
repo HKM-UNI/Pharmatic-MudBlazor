989ac6a302f957265bc4930c1df56dbcabef5faa
ChepeGit <jose.ramirez93u@std.uni.edu.ni>
2023-05-23 05:33:23 -0600

    Plantilla Diagramas MudBlazor
    Repare una cosita por ahi y añadi los componentes de charts de MudBlazor

--- Shared/CustomThemes.cs
+++ Shared/CustomThemes.cs
@@ -7,13 +7,9 @@ namespace Pharmatic.Shared
         public static Palette PharmaticPalette { get; } = new Palette()
         {
             Primary = "#17ACBE",
-            Surface = "#ECF1FF",
-            White = "#ECF1FF",
-            Background = "#ECF1FF",
-            BackgroundGrey = "#ECF1FF",
             PrimaryDarken = "#17ACBE",
             PrimaryLighten = "#17ACBE",
-            AppbarBackground = "#E6E6E6"
+            AppbarBackground = "#E6E6E6",
         };
 
         public static MudTheme PharmaticTheme { get; } = new MudTheme()
